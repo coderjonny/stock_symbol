d056c96fabe417e4cbc4a3739993989c24b89357
Jonny Kang <jonny@bypass.la>
2019-03-25 01:06:05 -0700

    Use callback to return data as a response

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ var http = require("http");
 var https = require("https");
 var server = http.createServer( (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-
   // console.log('create server , req', req);
-  
   // res.end(JSON.stringify({ a: 1 }, null, 3));
 
-
-  console.log(res);
+  // console.log(res);
   
+  // req.on('end')
 } );
 
-getQuote = (symbol) => {
+getQuote = (symbol, callbackFunc) => {
   // const symbolUrl = `https://api.iextrading.com/1.0/stock/${symbol}/quote`
+  // const logoUrl = https://api.iextrading.com/1.0/stock/aapl/logo
+  // const newsLink = https://api.iextrading.com/1.0/stock/aapl/news/last/1
   var options = {
     host: 'api.iextrading.com',
     path: `/1.0/stock/${symbol}/quote`,
@@ -21,7 +21,7 @@ getQuote = (symbol) => {
 };
   let data = "";
 
-  const getReq = https.get(options, res => {
+  const getReq = https.get(options, (res) => {
 
     console.log("api res.", res.statusCode);
 
@@ -34,9 +34,12 @@ getQuote = (symbol) => {
         try {
           const symbolData = JSON.parse(data)
           console.log("api call  ended!", symbolData.companyName);
+
+          callbackFunc(symbolData)
+
         } catch (e) {
           console.log('====================================');
-          console.log('error parsing JSON');
+          console.log('error parsing JSON', e);
           console.log('====================================');
         }
       } else {
@@ -55,7 +58,6 @@ getQuote = (symbol) => {
 
 server.on("request", (request, response) => {
   var body = [];
-
   const { headers, url, method } = request;
 
   let postData = "";
@@ -64,23 +66,45 @@ server.on("request", (request, response) => {
     postData += chunk;
   });
 
-  request
-    .on("end", () => {
+  request.on("end", () => {
+      body = Buffer.concat(body).toString();
       //'end' event is raised once per request,
       const {symbol} = JSON.parse(postData)
 
-      if (symbol) {
-        return getQuote(symbol)
+      response.on('err', err => {
+        console.log(err)
+      })
+      response.statusCode = 200;
+
+      response.setHeader('Content-Type', 'application/json');
+      // Note: the 2 lines above could be replaced with this next one:
+      // response.writeHead(200, {'Content-Type': 'application/json'})
+  
+      const responseBody = { headers, method, url, body };
+  
+      response.write(JSON.stringify(responseBody));
+      // response.end();
+      // Note: the 2 lines above could be replaced with this next one:
+      // response.end(JSON.stringify(responseBody))
+
+      const callback = (symbolData) => {
+        response.write(JSON.stringify(symbolData));
+        response.end();
       }
-    })
-    .on("error", () => {
+
+      if (symbol) {
+        getQuote(symbol, callback)
+      }
+
+    }).on("error", () => {
       response.statusCode = 400;
       response.end();
     });
 
   response.on("error", err => {
-    console.err(err);
+    console.log(err);
   });
+
 });
 
 server.listen(8080, () => {
