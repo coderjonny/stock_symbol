cb83c892ecc82d6f8ccbb392c8ed56a482370d4d
Jonny Kang <jonny@bypass.la>
2019-03-27 22:25:05 -0700

    adding the rest of the api calls

--- index.js
+++ index.js
@@ -2,26 +2,56 @@ var http = require("http");
 var https = require("https");
 var server = http.createServer( (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  // console.log('create server , req', req);
-  // res.end(JSON.stringify({ a: 1 }, null, 3));
-
-  // console.log(res);
-  
-  // req.on('end')
 } );
 
-getQuote = (symbol, callbackFunc) => {
-  // const symbolUrl = `https://api.iextrading.com/1.0/stock/${symbol}/quote`
-  // const logoUrl = https://api.iextrading.com/1.0/stock/aapl/logo
-  // const newsLink = https://api.iextrading.com/1.0/stock/aapl/news/last/1
+getNews = (symbol, callbackFunc) => {
   var options = {
     host: 'api.iextrading.com',
-    path: `/1.0/stock/${symbol}/quote`,
+    path: `/1.0/stock/${symbol}/news/last/1`,
     headers: {'User-Agent': 'request'}
 };
   let data = "";
 
-  const getReq = https.get(options, (res) => {
+  https.get(options, (res) => {
+    res.on("data", chunk => {
+      data += chunk;
+    });
+
+    res.on("end", () => {
+      if (res.statusCode === 200) {
+        try {
+          const symbolData = JSON.parse(data)
+          const newsLink = symbolData[0].url
+          callbackFunc({newsLink})
+
+        } catch (e) {
+          console.log('====================================');
+          console.log('error parsing JSON', e);
+          console.log('====================================');
+        }
+      } else {
+        console.log('====================================');
+        console.log('status', res.statusCode );
+        console.log('====================================');
+      }
+    });
+  }).on('error', err => {
+    console.log('====================================');
+    console.log('error: ', err);
+    console.log('====================================');
+  });
+
+}
+
+getLogo = (symbol, callbackFunc) => {
+  var options = {
+    host: 'api.iextrading.com',
+    path: `/1.0/stock/${symbol}/logo`,
+    headers: {'User-Agent': 'request'}
+  };
+  let data = "";
+
+  https.get(options, (res) => {
 
     console.log("api res.", res.statusCode);
 
@@ -33,9 +63,52 @@ getQuote = (symbol, callbackFunc) => {
       if (res.statusCode === 200) {
         try {
           const symbolData = JSON.parse(data)
-          console.log("api call  ended!", symbolData.companyName);
+          callbackFunc( {logoUrl: symbolData.url } )
+        } catch (e) {
+          console.log('====================================');
+          console.log('error parsing JSON', e);
+          console.log('====================================');
+        }
+      } else {
+        console.log('====================================');
+        console.log('status', res.statusCode );
+        console.log('====================================');
+      }
+    });
+  }).on('error', err => {
+    console.log('====================================');
+    console.log('error: ', err);
+    console.log('====================================');
+  });
 
-          callbackFunc(symbolData)
+}
+
+getQuote = (symbol, callbackFunc) => {
+  const options = {
+    host: 'api.iextrading.com',
+    path: `/1.0/stock/${symbol}/quote`,
+    headers: {'User-Agent': 'request'}
+  };
+
+  let data = "";
+  https.get(options, (res) => {
+    res.on("data", chunk => {
+      data += chunk;
+    });
+
+    res.on("end", () => {
+      if (res.statusCode === 200) {
+        try {
+          const symbolData = JSON.parse(data)
+
+          const {
+            latestPrice, companyName
+          } = symbolData
+
+          callbackFunc({
+            latestPrice,
+            companyName
+          })
 
         } catch (e) {
           console.log('====================================');
@@ -58,8 +131,6 @@ getQuote = (symbol, callbackFunc) => {
 
 server.on("request", (request, response) => {
   var body = [];
-  const { headers, url, method } = request;
-
   let postData = "";
 
   request.on("data", chunk => {
@@ -68,32 +139,41 @@ server.on("request", (request, response) => {
 
   request.on("end", () => {
       body = Buffer.concat(body).toString();
-      //'end' event is raised once per request,
       const {symbol} = JSON.parse(postData)
+      let responseObject = { }
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'application/json');
 
       response.on('err', err => {
         console.log(err)
       })
-      response.statusCode = 200;
 
-      response.setHeader('Content-Type', 'application/json');
-      // Note: the 2 lines above could be replaced with this next one:
-      // response.writeHead(200, {'Content-Type': 'application/json'})
-  
-      const responseBody = { headers, method, url, body };
-  
-      response.write(JSON.stringify(responseBody));
-      // response.end();
-      // Note: the 2 lines above could be replaced with this next one:
-      // response.end(JSON.stringify(responseBody))
+      const addResponseData = (data) => {
+        responseObject = {
+          ...responseObject,
+          ...data
+        }
+      }
 
-      const callback = (symbolData) => {
+      const endResponse = (symbolData) => {
         response.write(JSON.stringify(symbolData));
         response.end();
       }
 
       if (symbol) {
-        getQuote(symbol, callback)
+        getQuote(symbol, (quotePrice) => {
+            addResponseData(quotePrice)
+            getLogo(symbol, (logoData) => {
+                addResponseData(logoData)
+                getNews(symbol, (newsData) => {
+                  addResponseData(newsData)
+                  endResponse(responseObject)
+                })
+            })
+          }
+        )
+      } else {
+        endResponse({err: 'no symbol passed'})
       }
 
     }).on("error", () => {
